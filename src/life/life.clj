831c014b4b273960b9a469ddd38571a7e8b47b2e
Bruno Belarte <bruno.belarte@gmail.com>
2021-10-24 18:44:15 +0200

    Add print function

--- src/life/life.clj
+++ src/life/life.clj
@@ -1,4 +1,6 @@
-(ns life)
+(ns life
+  (:require [clojure.set :as set]
+            [clojure.string :as str]))
 
 (def pattern-eater
   ;; ---oo--oo
@@ -21,5 +23,38 @@
    :height 2
    :alive-cells #{[0 0] [1 0] [0 1] [1 1]}})
 
+(defn create-world [width height patterns]
+  (let [cells (->> patterns
+                   (map #(% :alive-cells))
+                   (apply set/union))]
+    {:width width
+     :height height
+     :alive-cells cells}))
+
 (defn create []
   (fn [board] board))
+
+(defn extract-x-axis [entry]
+  (->> entry
+       (map first)
+       (set)))
+
+(defn prepare-output [board]
+  (->> (board :alive-cells)
+       (group-by second)
+       (map (fn [[key value]] [key (extract-x-axis value)]))
+       (into {})))
+
+(defn draw-line [width array]
+  (str/join (map #(if (contains? array %) "#" "-") (range width))))
+
+(defn draw-world [board]
+  (let [y-axis (range (board :height))
+        output (prepare-output board)]
+    (str/join \newline (map #(if (contains? output %)
+                              (draw-line (board :width) (output %))
+                              (str/join (repeat (board :width) "-"))) y-axis))))
+
+(comment
+  (println (draw-world (create-world 9 6 [pattern-eater])))
+  (prepare-output pattern-eater))
