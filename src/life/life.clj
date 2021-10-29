d933e2818ad9a9c0e3b53823b6bc854221cc12a9
Bruno Belarte <bruno.belarte@gmail.com>
2021-10-29 17:04:08 +0200

    Add docstrings for public functions

--- src/life/life.clj
+++ src/life/life.clj
@@ -27,7 +27,9 @@
    :square
    #{[0 0] [1 0] [0 1] [1 1]}})
 
-(defn create-board [width height patterns]
+(defn create-board
+  "Creates a Game Of Life board. Requires a sequence of patterns."
+  [width height patterns]
   {:width width
    :height height
    :alive-cells (apply set/union patterns)})
@@ -47,26 +49,36 @@
                  :when (if ((board :alive-cells) coord) (survive? n) (birth? n))]
              coord))}))
 
-(def conway-stepper (stepper neighbours #{3} #{2 3}))
+(def conway-stepper
+  "Returns a default Conway's Game Of Life step function."
+  (stepper neighbours #{3} #{2 3}))
 
-(defn simulate [board iterations]
+(defn simulate
+  "Simulates n iterations of Conway's Game Of Life. Returns a sequence of all steps, including the initial board."
+  [board iterations]
   (->> (iterate conway-stepper board)
        (take (inc iterations))
        (last)))
 
-(defn shape-output [board alive dead]
+(defn- shape-output [board alive dead]
   (for [y (range (board :height))]
     (for [x (range (board :width))]
       (if (contains? (board :alive-cells) [x y]) alive dead))))
 
-(defn draw-board [board]
+(defn draw-board
+  "Draw the board as a multi-lines string."
+  [board]
   (let [output (shape-output board "#" "-")]
     (str/join \newline (map str/join output))))
 
-(defn offset [pattern coordinate]
+(defn offset
+  "Offset a pattern with the given coordinate."
+  [pattern coordinate]
   (set (map #(vector (+ (first %) (first coordinate)) (+ (second %) (second coordinate))) pattern)))
 
-(defn flip-x [pattern]
+(defn flip-x
+  "Flip a pattern on the X axis."
+  [pattern]
   (let [max-x (apply max (map first pattern))]
     (set (map #(vector (- max-x (first %)) (second %)) pattern))))
 
