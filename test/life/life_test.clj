6b085f17c0b9dfaab91a4417c37c5cc84623fd3d
Bruno Belarte <bruno.belarte@gmail.com>
2021-10-29 16:40:16 +0200

    Rewrite tests for readability

--- test/life/life_test.clj
+++ test/life/life_test.clj
@@ -2,37 +2,43 @@
   (:require [clojure.test :refer [deftest testing is]]
             [life.life :as life]))
 
+(def stepper life/conway-stepper)
+
+(def glider (life/patterns :glider))
+(def square (life/patterns :square))
+(def eater (life/patterns :eater))
+
+(def glider-board (life/create-board 4 4 [glider]))
+(def square-board (life/create-board 2 2 [square]))
+(def eater-board (life/create-board 9 6 [eater]))
+
 (deftest life-is-evolving-one-step-at-a-time
   (testing "Still life remains the same"
-    (let [stepper life/conway-stepper
-          eater (life/create-board 9 6 [(life/patterns :eater)])
-          square (life/create-board 2 2 [(life/patterns :square)])]
-      (is (= square (stepper square)))
-      (is (= eater (stepper eater)))))
+    (is (= square-board (stepper square-board)))
+    (is (= eater-board (stepper eater-board))))
   (testing "Glider evolves as expected"
-    (let [stepper life/conway-stepper
-          glider (life/create-board 4 4 [(life/patterns :glider)])
-          after-one-step (life/create-board 4 4 [#{[0 1] [2 1] [1 2] [2 2] [1 3]}])]
-      (is (= after-one-step (stepper glider)))))
+    (let [expected (life/create-board 4 4 [#{[0 1] [2 1] [1 2] [2 2] [1 3]}])]
+      (is (= expected (stepper glider-board)))))
   (testing "Blinker evolves as expected"
-    (let [stepper life/conway-stepper
-          blinker (life/create-board 3 3 [#{[0 1] [1 1] [2 1]}])
-          after-one-step (life/create-board 3 3 [#{[1 0] [1 1] [1 2]}])]
-      (is (= after-one-step (stepper blinker)))
+    (let [blinker (life/create-board 3 3 [#{[0 1] [1 1] [2 1]}])
+          expected (life/create-board 3 3 [#{[1 0] [1 1] [1 2]}])]
+      (is (= expected (stepper blinker)))
       (is (= blinker (stepper (stepper blinker)))))))
 
 (deftest life-is-evolving
   (testing "Glider keeps its shape after 4 steps"
-    (let [before (life/create-board 4 4 [(life/patterns :glider)])
-          after (life/create-board 4 4 [(life/offset (life/patterns :glider) [1 1])])]
-      (is (= after (life/simulate before 4))))))
+    (let [expected (life/create-board 4 4 [(life/offset glider [1 1])])]
+      (is (= expected (life/simulate glider-board 4))))))
 
 (deftest offset-pattern
   (testing "Offset glider"
-    (is (= (life/offset (life/patterns :glider) [1 2]) #{[2 2] [3 3] [3 4] [1 4] [2 4]})))
+    (let [expected #{[2 2] [3 3] [3 4] [1 4] [2 4]}]
+      (is (= expected (life/offset glider [1 2])))))
   (testing "Offset square"
-    (is (= (life/offset (life/patterns :square) [3 3]) #{[3 3] [3 4] [4 3] [4 4]}))))
+    (let [expected #{[3 3] [3 4] [4 3] [4 4]}]
+      (is (= expected (life/offset square [3 3]))))))
 
 (deftest flip-x
   (testing "Flipping glider on the x axis"
-    (is (= #{[1 0] [0 1] [0 2] [1 2] [2 2]} (life/flip-x (life/patterns :glider))))))
+    (let [expected #{[1 0] [0 1] [0 2] [1 2] [2 2]}]
+      (is (= expected (life/flip-x glider))))))
