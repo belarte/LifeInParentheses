f4fd10a6aafafa74be8644362d56cbbd7e1d8c76
Bruno Belarte <bruno.belarte@gmail.com>
2021-10-30 18:31:33 +0200

    Wire can be used to delay a bit

--- src/alu/alu.clj
+++ src/alu/alu.clj
@@ -7,7 +7,8 @@
   [value]
   {:pre [(or (= 1 value) (= 0 value))]}
   (let [pattern (if (zero? value) #{} (life/offset patterns/glider [1 1]))]
-    {:width 5
+    {:origin [0 0]
+     :width 5
      :height 5
      :output [3 3]
      :steps 0
@@ -21,5 +22,19 @@
         last (last steps)]
     (if (contains? (last :alive-cells) (expression :output)) 1 0)))
 
+(defn wire
+  "Allow transmission of one bit over a distance."
+  [expression distance]
+  (let [output (life/add-coords (expression :output) (vector distance distance))]
+    {:origin (expression :origin)
+     :width (max (expression :width) (+ 2 (- (first output) (first (expression :origin)))))
+     :height (max (expression :height) (+ 2 (- (second output) (second (expression :origin)))))
+     :output output
+     :steps (* 4 distance)
+     :pattern (expression :pattern)}))
+
 (comment
+  (let [test (wire (bit 1) 3)
+        board (life/create-board (test :width) (test :height) [(test :pattern)])]
+    (println (life/draw-board board)))
   (output (bit 1)))
--- test/alu/alu_test.clj
+++ test/alu/alu_test.clj
@@ -5,4 +5,7 @@
 (deftest write-and-read-bit
   (testing "A bit in input can be read as output"
     (is (= 0 (alu/output (alu/bit 0))))
-    (is (= 1 (alu/output (alu/bit 1))))))
+    (is (= 1 (alu/output (alu/bit 1)))))
+  (testing "A bit can be read after traversing a wire"
+    (is (= 0 (alu/output (alu/wire (alu/bit 0) 3))))
+    (is (= 1 (alu/output (alu/wire (alu/bit 1) 3))))))
