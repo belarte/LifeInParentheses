156cc280336193be4dd4c3ed82132f5e572ddcf4
Bruno Belarte <bruno.belarte@gmail.com>
2021-11-05 21:14:27 +0100

    Substract two coordinates

--- src/life/coords.clj
+++ src/life/coords.clj
@@ -5,6 +5,11 @@
   [left right]
   (vector (+ (first left) (first right)) (+ (second left) (second right))))
 
+(defn sub
+  "Substract two coordinates."
+  [[x0 y0] [x1 y1]]
+  [(- x0 x1) (- y0 y1)])
+
 (defn neighbours
   [[x y]]
   (for [dx [-1 0 1] dy [-1 0 1] :when (not= 0 dx dy)]
--- test/life/coords_test.clj
+++ test/life/coords_test.clj
@@ -12,6 +12,16 @@
     (is (= [0 0] (c/add [-2 -2] [2 2])))
     (is (= [-3 -3] (c/add [-1 -2] [-2 -1])))))
 
+(deftest sub
+  (testing "Substracting positive values"
+    (is (= [-2 -2] (c/sub [0 0] [2 2])))
+    (is (= [0 0] (c/sub [1 1] [1 1])))
+    (is (= [2 2] (c/sub [2 2] [0 0]))))
+  (testing "Substracting negative values"
+    (is (= [4 4] (c/sub [2 2] [-2 -2])))
+    (is (= [-4 -4] (c/sub [-2 -2] [2 2])))
+    (is (= [1 -1] (c/sub [-1 -2] [-2 -1])))))
+
 (deftest neighbours
   (testing "Neighbours at origin"
     (is (= #{[-1 -1] [-1 0] [-1 1] [0 -1] [0 1] [1 -1] [1 0] [1 1]} (set (c/neighbours [0 0])))))
