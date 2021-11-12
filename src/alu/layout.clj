b856947e33fe6d32124289b98737edebccc982b1
Bruno Belarte <bruno.belarte@gmail.com>
2021-11-12 13:40:51 +0100

    Align expressions for intersection function

--- src/alu/layout.clj
+++ src/alu/layout.clj
@@ -33,3 +33,66 @@
                 :direction direction}
        :steps (+ steps (* 4 distance))
        :pattern pattern})))
+
+(defn- shift [expression offset]
+  (let [origin (get-in expression [:dimensions :origin])
+        position (get-in expression [:output :position])
+        pattern (get expression :pattern)]
+    (-> expression
+        (assoc-in [:dimensions :origin] (coords/add origin offset))
+        (assoc-in [:output :position] (coords/add position offset))
+        (assoc :pattern (patterns/offset pattern offset)))))
+
+(defn- x-offset-at-origin [left right]
+  (let [x1 (get-in left [:dimensions :origin 0])
+        x2 (get-in right [:dimensions :origin 0])
+        width (get-in left [:dimensions :width])]
+    (- (+ x1 width) x2)))
+
+(defn- x-offset-at-output [left right]
+  (let [[x1] (get-in left [:output :position])
+        [x2] (get-in right [:output :position])]
+    (- x2 x1)))
+
+(defn- y-offset-at-output [left right]
+  (let [[_ y1] (get-in left [:output :position])
+        [_ y2] (get-in right [:output :position])]
+    (dec (- y1 y2))))
+
+(defn- delay-expression [left right]
+  (let [s1 (left :steps)
+        s2 (right :steps)
+        diff (int (/ (- s1 s2) 4))]
+    (if (pos? diff)
+      [left (wire right diff)]
+      [(wire left (- diff)) right])))
+
+(defn align-for-intersection
+  "Aligns expressions by shifting one so that outputs will intersect.
+  If both expression have a non zero output, they will cancel each other when intersecting.
+  First output will face bottom-right and second output will face bottom-left."
+  [left right]
+  {:pre [(not= ((left :output) :direction) ((right :output) :direction))]}
+  (if (= :bottom-left (get-in left [:output :direction]))
+    (align-for-intersection right left)
+    (let [[l r] (delay-expression left right)
+          x-min (x-offset-at-origin l r)
+          x-diff (x-offset-at-output l r)
+          x-offset (if (even? (+ x-min x-diff)) (inc x-min) x-min)
+          y-offset (y-offset-at-output l r)]
+      [l (shift r [x-offset y-offset])])))
+
+(comment
+  (let [init {:dimensions {:origin [0 0]
+                           :width 5
+                           :height 5}
+              :output {:position [3 3]
+                       :direction :bottom-right}
+              :steps 0
+              :pattern (patterns/offset patterns/glider [1 1])}
+        shifted (shift init [2 1])]
+    (println init)
+    (println (shift init [2 1]))
+    (println (x-offset-at-origin shifted init))
+    (println (x-offset-at-output init shifted))
+    (println (y-offset-at-output init shifted))))
--- test/alu/layout_test.clj
+++ test/alu/layout_test.clj
@@ -23,3 +23,41 @@
       (is (= :bottom-left (output :direction)))
       (is (= 16 steps))
       (is (= #{[6 1] [5 3] [1 4]} pattern)))))
+
+(deftest align-for-intesection
+  (let [left {:dimensions {:origin [2 2]
+                           :width 6
+                           :height 5}
+              :output {:position [4 5]
+                       :direction :bottom-left}
+              :steps 24}
+        right {:dimensions {:origin [4 1]
+                            :width 4
+                            :height 4}
+               :output {:position [6 3]
+                        :direction :bottom-right}
+               :steps 16}
+        [left-output right-output] (layout/align-for-intersection left right)]
+    (testing "Result expressions do not overlap"
+      (let [[xl] (get-in left-output [:dimensions :origin])
+            [xr] (get-in right-output [:dimensions :origin])
+            w (get-in left-output [:dimensions :width])]
+        (is (>= xr (+ xl w)))))
+    (testing "X origin coordinates are separated by an even number of cells"
+      (let [[xl] (get-in left-output [:output :position])
+            [xr] (get-in right-output [:output :position])]
+        (is (odd? (- xr xl)))))
+    (testing "Both result expressiona have same number of steps"
+      (let [left-steps (left-output :steps)
+            right-steps (right-output :steps)]
+        (is (= left-steps right-steps))))
+    (testing "Outputs align"
+      (let [[_ yl] (get-in left-output [:output :position])
+            [_ yr] (get-in right-output [:output :position])]
+        (is (= yr (- yl 1)))))
+    (testing "Throws exception if outputs have the same direction"
+      (is (thrown? AssertionError (layout/align-for-intersection left left)))
+      (is (thrown? AssertionError (layout/align-for-intersection right right))))
+    (testing "Organise epressions so first is going bottom-right and second bottom-left"
+      (is (= :bottom-right (get-in left-output [:output :direction])))
+      (is (= :bottom-left (get-in right-output [:output :direction]))))))
