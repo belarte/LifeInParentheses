ceec5b36bd1350dd3ea039f6818952485d0ca1da
Bruno Belarte <bruno.belarte@gmail.com>
2021-12-22 11:33:28 +0100

    Intercept ctrl-c to gracefully shutdown

--- src/app/main.clj
+++ src/app/main.clj
@@ -1,6 +1,17 @@
 (ns app.main
+  (:require [clojure.tools.logging :as log]
+            [clojure.repl :as repl])
   (:gen-class))
+
+(defn shutdown!
+  [& _]
+  (log/info "Received SIGINT, Shutting down")
+  (shutdown-agents)
+  (System/exit 0))
 
 (defn -main
   [& _]
-  (println (+ 1 2)))
+  (repl/set-break-handler! shutdown!)
+  (println (+ 1 2))
+  (Thread/sleep 5000)
+  (println "Done"))
