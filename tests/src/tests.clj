a5e48289eae52f922d1c11679e24b73f589a54b5
Bruno Belarte <bruno.belarte@gmail.com>
2022-05-24 21:20:01 +0200

    Create sub folder for integration tests

---
+++ tests/src/tests.clj
@@ -0,0 +1,49 @@
+#!/usr/bin/env bb
+
+(ns tests
+  (:require [clojure.string :as string]
+            [clojure.tools.cli :refer [parse-opts]]))
+
+(def cli-options
+  [["-p" "--port PORT" "Port number"
+    :default 3000
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
+   [nil "--host HOST" "Host"
+    :default "localhost"]
+   ["-h" "--help" "Print this help"]])
+
+(defn- usage [options-summary]
+  (->> ["Integration tests for LifeInParentheses."
+        ""
+        "Usage: bb run-tests [options] calculator.jar"
+        ""
+        "Options:"
+        options-summary]
+       (string/join \newline)))
+
+(defn- error-msg [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (string/join \newline errors)))
+
+(defn- validate-args
+  "Validate command line arguments. Either return a map indicating the program
+  should exit (with an error message, and optional ok status), or a map
+  indicating the action the program should take and the options provided."
+  [args]
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+    (cond
+      (:help options)         {:exit-message (usage summary) :ok? true}
+      errors                  {:exit-message (error-msg errors)}
+      (= 1 (count arguments)) {:file (first arguments) :options options}
+      :else                   {:exit-message (usage summary)})))
+
+(defn- exit [status msg]
+  (println msg)
+  (System/exit status))
+
+(defn -main [& args]
+  (let [{:keys [file options exit-message ok?]} (validate-args args)]
+    (if exit-message
+      (exit (if ok? 0 1) exit-message)
+      (exit 0 (str "Processing file " file " with options " options)))))
