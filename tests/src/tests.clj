bbed7b6a03fc7c53bd508ac7cf849b1148a95adf
Bruno Belarte <bruno.belarte@gmail.com>
2022-08-01 17:32:10 +0200

    Extract function to call endpoint

--- tests/src/tests.clj
+++ tests/src/tests.clj
@@ -11,15 +11,19 @@
     (Thread/sleep 3000)
     proc))
 
+(defn- call-endpoint [host port]
+  (let [addr   (str host ":" port "/health")
+        health (curl/get addr {:throw false})
+        status (:status health)]
+    (if (= 200 status)
+      [0 "Success!"]
+      [1 (str "Cannot check health, error=" (:err health))])))
+
 (defn run [file options]
   (println (str "Processing file " file " with options " options))
-  (let [port (:port options)
-        host (:host options)
-        addr (str host ":" port "/health")]
-    (when-not (:no-startup options)
+  (let [port  (:port options)
+        host  (:host options)
+        start (not (:no-startup options))]
+    (when start
       (start-calculator file port))
-    (let [health (curl/get addr {:throw false})
-          status (:status health)]
-      (if (= 200 status)
-        [0 "Success!"]
-        [1 (str "Cannot check health, error=" (:err health))]))))
+    (call-endpoint host port)))
