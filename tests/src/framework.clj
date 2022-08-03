4f0786a6803483ba2b4884b58c618036e17adb12
Bruno Belarte <bruno.belarte@gmail.com>
2022-08-03 15:59:13 +0200

    Split file

---
+++ tests/src/framework.clj
@@ -0,0 +1,40 @@
+(ns framework
+  (:require [babashka.curl :as curl]
+            [babashka.process :refer [process destroy-tree]]
+            [babashka.wait :refer [wait-for-port]]
+            [clojure.string :as s]
+            [cheshire.core :as json]))
+
+(defn humanized [error]
+  (-> error :body (json/parse-string true) :humanized))
+
+(defn pretty-error [{:keys [name endpoint params result]}]
+  {:name     name
+   :endpoint endpoint
+   :params   params
+   :error    (humanized result)})
+
+(defn start-calculator [file port]
+  (let [cmd  ["java" "-jar" file "--port" port]
+        proc (process cmd {:out :inherit
+                           :shutdown destroy-tree})]
+    (println (str "Starting calculator: `" (s/join " " cmd) "'"))
+    (wait-for-port "localhost" port {:timeout 30000 :pause 500})
+    proc))
+
+(defn create-endpoint [host port]
+  (fn [{:keys [endpoint params test-fn]}]
+    (let [url (str host ":" port endpoint)
+          res (curl/get url {:query-params params :throw false})]
+      (if (test-fn res) {} res))))
+
+(comment
+  (let [call-endpoint (create-endpoint "localhost" "3000")]
+    (call-endpoint {:endpoint "/calculate"
+                    :params {"expression" "3&7"}
+                    :test-fn #(= 200 (:status %))}))
+  (->
+    (curl/get "localhost:3000/calculate" {:query-params {} :throw false})
+    :body
+    (json/parse-string true)
+    :humanized))
--- tests/src/tests.clj
+++ tests/src/tests.clj
@@ -1,18 +1,5 @@
 (ns tests
-  (:require [babashka.process :refer [process destroy-tree]]
-            [babashka.curl :as curl]
-            [babashka.wait :refer [wait-for-port]]
-            [clojure.string :as s]
-            [cheshire.core :as json]))
-
-(defn- humanized [error]
-  (-> error :body (json/parse-string true) :humanized))
-
-(defn- pretty-error [{:keys [name endpoint params result]}]
-  {:name     name
-   :endpoint endpoint
-   :params   params
-   :error    (humanized result)})
+  (:require [framework :refer [humanized pretty-error create-endpoint start-calculator]]))
 
 (def test-cases
   [{:name     "Health check"
@@ -31,20 +18,6 @@
                  (= ["missing required key"]
                     ((humanized %) :expression)))}])
 
-(defn- start-calculator [file port wait]
-  (let [cmd  ["java" "-jar" file "--port" port]
-        proc (process cmd {:out :inherit
-                           :shutdown destroy-tree})]
-    (println (str "Starting calculator: `" (s/join " " cmd) "'"))
-    (wait)
-    proc))
-
-(defn- create-endpoint [host port]
-  (fn [{:keys [endpoint params test-fn]}]
-    (let [url (str host ":" port endpoint)
-          res (curl/get url {:query-params params :throw false})]
-      (if (test-fn res) {} res))))
-
 (defn- run-tests [call-endpoint]
   (let [res  (->> test-cases
                   (map #(assoc % :result (call-endpoint %)))
@@ -57,18 +30,6 @@
   (let [port  (:port options)
         host  (:host options)
         start (not (:no-startup options))
-        wait  #(wait-for-port host port {:timeout 30000 :pause 500})
         call-endpoint (create-endpoint host port)]
-    (when start (start-calculator file port wait))
+    (when start (start-calculator file port))
     (run-tests call-endpoint)))
-
-(comment
-  (let [call-endpoint (create-endpoint "localhost" "3000")]
-    (call-endpoint {:endpoint "/calculate"
-                    :params {"expression" "3&7"}
-                    :test-fn #(= 200 (:status %))}))
-  (->
-    (curl/get "localhost:3000/calculate" {:query-params {} :throw false})
-    :body
-    (json/parse-string true)
-    :humanized))
