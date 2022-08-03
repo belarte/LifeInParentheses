f37807a9251aca64696d40855479a896cc901533
Bruno Belarte <bruno.belarte@gmail.com>
2022-08-03 15:42:59 +0200

    Integration test framework handles multiple tests gracefully

--- tests/src/tests.clj
+++ tests/src/tests.clj
@@ -5,6 +5,32 @@
             [clojure.string :as s]
             [cheshire.core :as json]))
 
+(defn- humanized [error]
+  (-> error :body (json/parse-string true) :humanized))
+
+(defn- pretty-error [{:keys [name endpoint params result]}]
+  {:name     name
+   :endpoint endpoint
+   :params   params
+   :error    (humanized result)})
+
+(def test-cases
+  [{:name     "Health check"
+    :endpoint "/health"
+    :test-fn  #(= 200 (:status %))}
+
+   {:name     "Calculate endpoint is responsive"
+    :endpoint "/calculate"
+    :params   {"expression" "0"}
+    :test-fn  #(= 200 (:status %))}
+
+   {:name     "Calculate endpoint requires an expression"
+    :endpoint "/calculate"
+    :test-fn  #(and
+                 (= 400 (:status %))
+                 (= ["missing required key"]
+                    ((humanized %) :expression)))}])
+
 (defn- start-calculator [file port wait]
   (let [cmd  ["java" "-jar" file "--port" port]
         proc (process cmd {:out :inherit
@@ -14,17 +40,17 @@
     proc))
 
 (defn- create-endpoint [host port]
-  (fn create
-    ([endpoint]
-     (create endpoint {}))
-    ([endpoint params]
-     (let [url    (str host ":" port endpoint)
-           health (curl/get url {:query-params params :throw false})
-           status (:status health)]
-       (println (str "Calling " url) (if (empty? params) "" params))
-       (if (= 200 status)
-         [0 "Success!"]
-         [1 (str "Status not ok, error=" (:err health))])))))
+  (fn [{:keys [endpoint params test-fn]}]
+    (let [url (str host ":" port endpoint)
+          res (curl/get url {:query-params params :throw false})]
+      (if (test-fn res) {} res))))
+
+(defn- run-tests [call-endpoint]
+  (let [res  (->> test-cases
+                  (map #(assoc % :result (call-endpoint %)))
+                  (filter #(seq (:result %)))
+                  (map pretty-error))]
+    [(count res) (if (empty? res) "Success!" res)]))
 
 (defn run [file options]
   (println (str "Processing file " file " with options " options))
@@ -34,13 +60,15 @@
         wait  #(wait-for-port host port {:timeout 30000 :pause 500})
         call-endpoint (create-endpoint host port)]
     (when start (start-calculator file port wait))
-    (call-endpoint "/health")
-    (call-endpoint "/calculate" {"expression" "3&7"})))
+    (run-tests call-endpoint)))
 
 (comment
   (let [call-endpoint (create-endpoint "localhost" "3000")]
-    (call-endpoint "/calculate" {"expression" "3&7"}))
+    (call-endpoint {:endpoint "/calculate"
+                    :params {"expression" "3&7"}
+                    :test-fn #(= 200 (:status %))}))
   (->
-    (curl/get "localhost:3000/calculate" {:query-params {"expression" "3&7"} :throw false})
+    (curl/get "localhost:3000/calculate" {:query-params {} :throw false})
     :body
-    (json/parse-string true)))
+    (json/parse-string true)
+    :humanized))
