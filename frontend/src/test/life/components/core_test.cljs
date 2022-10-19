8c95d9196a362453b33257ff31a2e96a07025135
Bruno Belarte <bruno.belarte@gmail.com>
2022-10-19 16:53:20 +0200

    Use React Testing Library for integration tests

--- frontend/src/test/life/components/core_test.cljs
+++ frontend/src/test/life/components/core_test.cljs
@@ -1,5 +1,25 @@
 (ns life.components.core-test
-  (:require [cljs.test :refer [deftest is]]))
+  (:require [cljs.test :refer [deftest testing is use-fixtures]]
+            [reagent.core :as r]
+            ["@testing-library/react" :as rtl]
+            [life.components.core :as component]))
 
-(deftest a-test
-  (is (= 1 1)))
+(use-fixtures :each {:after rtl/cleanup})
+
+(defn with-mounted-component [component f]
+  (let [mounted-component (rtl/render (r/as-element component))]
+    (try
+      (f mounted-component)
+      (finally
+        (.unmount mounted-component)
+        (r/flush)))))
+
+(deftest title-component
+  (testing "A title is displayed"
+    (with-mounted-component
+      [component/title]
+      (fn [component]
+        (is (= "Life in parenthesis"
+               (-> component
+                   (.getByText #"(?i)life")
+                   (.-innerHTML))))))))
