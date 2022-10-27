c08f170edc3f4c5fcc02ff77036de5feb5ebca79
Bruno Belarte <bruno.belarte@gmail.com>
2022-10-27 11:10:57 +0200

    Draw next iteration on click

--- frontend/src/main/life/components/canvas.cljs
+++ frontend/src/main/life/components/canvas.cljs
@@ -2,19 +2,27 @@
   (:require [reagent.core :as r]))
 
 (defonce canvas-ref (r/atom nil))
-
 (defonce steps (r/atom []))
+(defonce counter (r/atom 0))
 
 (def size 5)
 
+(defn- next-step []
+  (let [last-index (- (count @steps) 1)
+        index      (min @counter last-index)]
+    (get @steps index)))
+
 (defn- draw []
   (let [ctx (.getContext @canvas-ref "2d")]
+    (.clearRect ctx 0 0 (.-width @canvas-ref) (.-height @canvas-ref))
     (run!
       (fn [[x y]] (.fillRect ctx (* x size) (* y size) size size))
-      (get @steps 0))))
+      (next-step)))
+  (swap! counter inc))
 
 (defn canvas [w h s]
   (reset! steps s)
+  (reset! counter 0)
   [:div
    [:div
     [:canvas {:id "canvas"
